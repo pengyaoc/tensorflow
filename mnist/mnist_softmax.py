472cd0a037577f806e8f5bbb78f028c04c7cd9ff
Pengyao Chen <pengyaoc@gmail.com>
2017-06-18 12:19:03 -0700

    [minst] implement softmax_cross_entropy_with_logits by hand

--- mnist/mnist_softmax.py
+++ mnist/mnist_softmax.py
@@ -6,11 +6,11 @@ x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
 
-y = tf.matmul(x, W) + b
+y = tf.nn.softmax(tf.matmul(x, W) + b)
 
 y_ = tf.placeholder(tf.float32, [None, 10])
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
